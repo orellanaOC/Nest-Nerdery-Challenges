a654142bc1eec9caaa00bfab049d65e8ad37282a
Concepcion Orellana <oc.concepcionorellana@gmail.com>
2024-10-03 11:13:11 -0600

    feat: add JWT guards

---
+++ src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,17 @@
+import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+	private readonly logger = new Logger(JwtAuthGuard.name);
+
+	constructor(private reflector: Reflector) {
+		super();
+	}
+
+	canActivate(context: ExecutionContext) {
+		this.logger.log('Activating JWT Guard');
+		return super.canActivate(context);
+	}
+}
